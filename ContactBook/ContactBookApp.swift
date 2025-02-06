99c7ed279370bad31b6a8ce230fa2c60dd9735ed
AlbertoJTD <albertojtd@outlook.com>
2025-02-05 22:52:58 -0600

    Initial Commit

---
+++ ContactBook/ContactBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactBookApp.swift
+//  ContactBook
+//
+//  Created by Alberto Jesus Tepale Diagas on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactBookTests/ContactBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactBookTests.swift
+//  ContactBookTests
+//
+//  Created by Alberto Jesus Tepale Diagas on 05/02/25.
+//
+
+import Testing
+@testable import ContactBook
+
+struct ContactBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
